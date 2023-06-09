c54140f91862a7e626999b0dd50bedd572c05a04
treijan <madelo.angelaizy.f@gmail.com>
2023-06-09 19:10:28 +0800

    v1.0 web

---
+++ zy.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal ({
+	distance: '65px',
+	duration: 2600,
+	delay: 450,
+	reset: true
+});
+
+sr.reveal('.hero-text',{delay:200, origin:'top'});
+sr.reveal('.hero-img',{delay:450, origin:'top'});
+sr.reveal('.icons',{delay:500, origin:'left'});
